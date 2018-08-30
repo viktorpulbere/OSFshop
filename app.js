7ffeb4dc20026cdedd023f9070c393e2e9d6f35b
victorpulbere <vicktor.pulbere@gmail.com>
2018-08-30 19:53:38 +0300

    third commit

---
+++ app.js
@@ -0,0 +1,99 @@
+var express = require('express');
+var path = require('path');
+var favicon = require('serve-favicon');
+var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var expressHbs = require('express-handlebars');
+var mongoose = require('mongoose');
+var routes = require('./routes/index');
+var mensRoute = require('./routes/mens');
+var womensRoute = require('./routes/womens');
+var userRoute = require('./routes/user');
+var newRoute = require('./routes/thumbnails');
+const Handlebars = require('handlebars');
+var session = require('express-session');
+var passport = require('passport');
+var flash = require('connect-flash');
+var validator = require('express-validator');
+var MongoStore = require ('connect-mongo')(session);
+
+var app = express();
+
+Handlebars.registerHelper('ifCond', function(v1, v2, options) {
+  if(v1 === v2) {
+    return options.fn(this);
+  }
+  return options.inverse(this);
+});
+
+mongoose.connect('mongodb://localhost:27017/shop', { useNewUrlParser: true });
+var db = mongoose.connection;
+require('./config/passport');
+//mongo error
+db.on('error', console.error.bind(console,'connection error:'));
+
+// view engine setup
+app.engine('.hbs', expressHbs({defaultLayout: 'layout', extname: '.hbs'}));
+app.set('view engine', '.hbs');
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(validator());
+app.use(cookieParser());
+app.use(session({
+  secret: 'mysupersecret', 
+  resave: false, 
+  saveUninitialized: false,
+  store: new MongoStore({ mongooseConnection: mongoose.connection}),
+  cookies: { maxAge: 180*60*1000 }
+}));
+app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next) {
+  res.locals.login = req.isAuthenticated();
+  res.locals.session = req.session;
+  next();
+});
+
+app.use('/user',userRoute);
+app.use('/', routes, mensRoute, womensRoute, newRoute);
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+  res.status(err.status || 500);
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
+});
+
+app.listen(3000);
+
+module.exports = app;
